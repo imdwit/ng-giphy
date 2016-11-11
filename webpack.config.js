7a2fc040aa22357a5d8c97bcd59a3a13c6c2d9b9
Dwight Doane <dwight.doane@startupmedia.com>
2016-11-10 20:27:54 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  // This code will be compiled by webpack according to the babel specifications
+  entry: "./src/app.js",
+  // The plain compiled Javascript will be output into this file
+  output: {
+    filename: "public/bundle.js"
+  },
+  // This will be what we do
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_moduled|bower_components)/,
+        loader: 'babel',
+        query: {
+          // These are the specific transformations we'll be using.
+          presets: ['es2015'],
+        }
+      }
+    ]
+  }
+}
