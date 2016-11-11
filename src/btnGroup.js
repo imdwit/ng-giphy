60148df02d370df59352538d01b45f1f5e37337c
Dwight Doane <dwight.doane@startupmedia.com>
2016-11-10 20:28:32 -0500

    button group

---
+++ src/btnGroup.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+var buttonGroup = {
+  bindings: {
+    topics: '<',
+    handleClick: '&',
+  },
+  template: `
+    <div id="buttons">
+      <button
+        ng-repeat="topic in $ctrl.topics track by $index"
+        ng-click="$ctrl.handleClick({$event: {topic: topic}})">
+        {{topic}}
+      </button>
+    </div>
+  `
+};
+
+const buttonGroupModule = angular.module('app.buttonGroup', []);
+
+buttonGroupModule.component('buttonGroup', buttonGroup);
+
+export default buttonGroupModule;
