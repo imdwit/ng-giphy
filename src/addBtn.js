7745689ad528eea860cb97b9bb563196dd86c126
Dwight Doane <dwight.doane@startupmedia.com>
2016-11-10 20:28:09 -0500

    addBtn module

---
+++ src/addBtn.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+var addBtn = {
+  bindings: {
+    handleKeyUp: '&',
+  },
+  template: `
+    <input ng-model="$ctrl.topic" ng-keyup="$ctrl._handleKeyUp($event)" />
+  `,
+  controller: function() {
+    this.$onInit = function() {
+      this.topic = '';
+    };
+
+    this._handleKeyUp = function(e) {
+      console.log(e);
+      if (e.which === 13) {
+        this.handleKeyUp({$event: {topic: this.topic}});
+      }
+    }
+  }
+};
+
+const addBtnModule = angular.module('app.addBtn', []);
+
+addBtnModule.component('addBtn', addBtn);
+
+export default addBtnModule;
