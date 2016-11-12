55fd1d277db43b2489090fc8d1ac47172fa80a80
Dwight <dwightdoane@me.com>
2016-11-12 13:44:56 -0500

    added a filter by rating

--- src/gifs.js
+++ src/gifs.js
@@ -4,15 +4,17 @@ var gifsList = {
   bindings: {
     gifs: '<',
     handleClick: '&',
+    filterBy: '<',
   },
   template: `
     <div id="gifs">
       <div
         class="gif"
-        ng-repeat="gif in $ctrl.gifs track by $index"
+        ng-repeat="gif in $ctrl.gifs | filter:$ctrl.filterBy"
         ng-click="$ctrl.handleClick({$event: {gif: gif}})">
           <img class="gif__img" ng-if="gif.playing" ng-src="{{gif.original}}" />
           <img class="gif__img" ng-if="!gif.playing" ng-src="{{ gif.still }}" />
+          <p>Rating: {{gif.rating}}</p>
       </div>
     </div>
   `
